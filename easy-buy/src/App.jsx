80fda2bdd1edf1c27891ef2bc0abbda547265407
MatheusKemuel <matheusbr209@gmail.com>
2023-07-11 18:41:25 -0300

    Top bar quase finalizada

--- easy-buy/src/App.jsx
+++ easy-buy/src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import { TopBar } from "./components/TopBar";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <TopBar />
     </>
   );
 }
---
+++ easy-buy/src/components/TopBar.jsx
@@ -0,0 +1,61 @@
+import { FcLike } from "react-icons/fc";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { BiUser } from "react-icons/bi";
+import { BsSearch } from "react-icons/bs";
+import logo from "../assets/images/logo.png";
+
+export const TopBar = () => {
+  return (
+    <div className="topBarContainer flex items-center h-20 bg-blue-950">
+      <div className="logo flex items-center text-orange-400 text-xl font-bold ml-10">
+        <img src={logo} alt="" className="w-20" />
+        <p>EasyBuy</p>
+      </div>
+
+      <select
+        id="countries"
+        className=" text-xl border-none text-gray-900 rounded-lg ml-20 block w-23 h-20 p-2.5 dark:bg-blue-950 dark:border-none dark:placeholder-gray-400 dark:text-white "
+      >
+        <option selected>Categorias</option>
+        <option value="fone">Fone</option>
+        <option value="teclado">Teclado</option>
+        <option value="mouse">Mouse</option>
+      </select>
+      <form className="w-full">
+        <input className="search-bar h-10 rounded-3xl ml-7 w-full p-5" type="text" placeholder="Pesquisar..." />
+      </form>
+      <span className="cursor-pointer">
+        <BsSearch />
+      </span>
+
+      <ul className="flex mr-10">
+        <li>
+          <a
+            className="flex items-center gap-1 ml-10 text-xl text-white"
+            href=""
+          >
+            Favoritos <FcLike />
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center gap-1 ml-10 text-xl text-white"
+            href=""
+          >
+            Carrinho <AiOutlineShoppingCart />
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center gap-1 ml-10 text-xl text-white"
+            href=""
+          >
+            Conta <BiUser />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default TopBar;
